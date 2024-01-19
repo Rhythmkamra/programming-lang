682290051e845e28a46f100a4c0459ab4153ae75
Rhythmkamra <itsmerhythmk@gmail.com>
2024-01-19 13:04:11 +0530

    add

---
+++ javascript/dom.js
@@ -0,0 +1,6 @@
+// lecture 1
+// html document has been divided into parts ie the html the Headers,body and elements and the elements further Have 
+// attributes and values which are also termed as nodes 
+// when we use the console of the browser we can access it elements via 
+// their specifications and javascript menthod 
+// document.getElementById,many more specifications and can update the existing data on the html page 
