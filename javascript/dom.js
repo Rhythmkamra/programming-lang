b2af63c27c8f93246c1858d137a0248508ad7996
Rhythmkamra <itsmerhythmk@gmail.com>
2024-01-19 14:29:35 +0530

    add

--- javascript/dom.js
+++ javascript/dom.js
@@ -4,3 +4,15 @@
 // when we use the console of the browser we can access it elements via 
 // their specifications and javascript menthod 
 // document.getElementById,many more specifications and can update the existing data on the html page 
+
+
+
+// innerText= displays only the visible part of the html doc
+// innerText = shows the whole text that is somehow hidden via any other property
+
+
+// querySelector :- selects the first element fetched
+// querySelector :- selects all the present elements
+
+// node and HTMLAllCollection are not pure array
+ 
